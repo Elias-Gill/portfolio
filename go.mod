2457d9c92c1511cb1bad08ca71be092743890a39
Elias Gill <eliasgill42@gmail.com>
2024-11-21 13:04:11 -0300

    feat: post list endpoint squeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/elias-gill/portfolio
 
-go 1.21.0
+go 1.22.0
+
+require github.com/yuin/goldmark v1.7.8 // indirect
--- main.go
+++ main.go
@@ -4,22 +4,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
-func templateFromBase(w http.ResponseWriter, names ...string) (*template.Template, error) {
+func templateFromBase(names ...string) (*template.Template, error) {
 	templates := append(names, "./templates/base.html", "./templates/footer.html", "./templates/navbar.html")
 
 	tmpl, err := template.ParseFiles(templates...)
 	if err != nil {
-		http.Error(w, "Error parsing template", http.StatusInternalServerError)
 		log.Print(err.Error())
 		return nil, err
 	}
 
 	return tmpl, nil
 }
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := templateFromBase(w, "./templates/home.html", "./templates/projects.html", "./templates/tecnologias.html")
+
+func serveAboutMe(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := templateFromBase("./pages/home/home.html", "./pages/home/projects.html",
+		"./pages/home/tecnologias.html")
 	if err != nil {
 		http.Error(w, "Error parsing template", http.StatusInternalServerError)
 		return
@@ -30,13 +32,32 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func servePostsList(w http.ResponseWriter, r *http.Request) {
+	// TODO: Parsear los metadatos y sacar los titulos
+	posts, err := os.ReadDir("./posts")
+	if err != nil {
+		log.Fatal("Cannot open posts folder")
+	}
+
+	for _, file := range posts {
+		w.Write([]byte(file.Name()))
+	}
+}
+
 func main() {
 	// route for serving static files
 	assets_path := http.FileServer(http.Dir("./assets"))
-	http.HandleFunc("/", serveTemplate)
-
 	http.Handle("/assets/", http.StripPrefix("/assets/", assets_path))
 
-	print("serving in port 8000:\n")
-	http.ListenAndServe(":8000", nil)
+	// pages
+	http.HandleFunc("/", serveAboutMe)
+	http.HandleFunc("/posts/", servePostsList)
+	// http.HandleFunc("/posts/{id}/", servePostDetail)
+
+	log.Print("Starting server...\n")
+	log.Print("Serving in port 8000\n")
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatalf("Cannot initialize server on port 8000: %s", err.Error())
+	}
 }
