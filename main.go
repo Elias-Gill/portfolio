4db78c39af6882c1c986bcdf9bb3275a2e2d8827
Elias Gill <eliasgill42@gmail.com>
2025-02-03 15:21:25 -0300

    feat: enhancing search engine indexation

--- main.go
+++ main.go
@@ -46,10 +46,21 @@ func main() {
 	http.HandleFunc("/posts/", serveBlog)
 	http.HandleFunc("/posts/{post}/", servePostDetail)
 
+	// search engines indexing
+	http.HandleFunc("/robots.txt", robotsHandler)
+
 	log.Print("Starting server...\n")
 	log.Printf("Serving in port %s\n", port)
 
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalf("Cannot initialize server on port %s %s", port, err.Error())
 	}
+}
+
+func robotsHandler(w http.ResponseWriter, r *http.Request) {
+	robotsTxt := `User-agent: *
+	Allow: /
+	`
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(robotsTxt))
 }
